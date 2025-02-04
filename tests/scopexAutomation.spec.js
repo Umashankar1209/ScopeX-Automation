bbc8045e6a18599c8a0e6c947ccaf91949b6ad70
Umashankar <Umashankar.t1996@gmail.com>
2025-02-04 12:03:36 +0530

    Data Driven Commit

--- tests/scopexAutomation.spec.js
+++ tests/scopexAutomation.spec.js
@@ -1,21 +1,33 @@
 import {test,expect} from '@playwright/test'
+import fs from 'fs';
+import path from 'path';
+import { parse } from 'csv-parse/sync';
 
-// Test data
+const records = parse(
+  fs.readFileSync(path.join(__dirname, "../TestDataScopeX/TestDataScopeX.csv")),
+  {
+      columns: true,
+      skip_empty_lines: true
+  }
+);
+test.describe("ScopeX Automation", () => {
+  records.forEach((record) => {
+/* Test data
 const USER_EMAIL = 'Umashankar.t1996@gmail.com';
 const USER_PASSWORD = 'shankar*1A';
 const RECIPIENT_NAME = 'Umashankar';
 const RECIPIENT_NICKNAME = 'SHANKAR';
 const bankaccountnumber = '50100321820494';
 const bankifscnumber = 'HDFC0002240';
-
-test('Scopex Money End-to-End Test', async ({ page }) => {
+*/
+test(`Scopex Money End-to-End Test ${record.TestCaseId}`, async ({ page }) => {
   // Step 1: Navigate to the login page
   await page.goto('https://scopex.money/Login');
   await page.screenshot({ path: 'screenshots/login_page.png' });
 
   // Step 2: Login with registered user credentials
-  await page.fill("//input[@id=':r3:-form-item']", USER_EMAIL);
-  await page.fill("//input[@id=':r4:-form-item']", USER_PASSWORD);
+  await page.fill("//input[@id=':r3:-form-item']",record.UserEmail);
+  await page.fill("//input[@id=':r4:-form-item']",record.UserPassword);
   await page.click("//button[normalize-space()='Log in']");
   await page.screenshot({ path: 'screenshots/after_login.png' });
 
@@ -26,10 +38,10 @@ test('Scopex Money End-to-End Test', async ({ page }) => {
   // Step 3: Add a recipient
   await page.click("//span[@class='flex-1 ml-3 text-left whitespace-nowrap']");
   await page.click("//a[normalize-space()='Add Recipient']");
-  await page.fill("//input[@placeholder='Enter recipient name']", RECIPIENT_NAME);
-  await page.fill("//input[@placeholder='Enter recipient nick name']", RECIPIENT_NICKNAME);
-  await page.fill("//input[@placeholder='Enter bank account number']",bankaccountnumber)
-  await page.fill("//input[@placeholder='Enter IFSC code']",bankifscnumber);
+  await page.fill("//input[@placeholder='Enter recipient name']",record.RecipientName);
+  await page.fill("//input[@placeholder='Enter recipient nick name']",record.RecipientNickName);
+  await page.fill("//input[@placeholder='Enter bank account number']",record.BankAccountNumber)
+  await page.fill("//input[@placeholder='Enter IFSC code']",record.IFSCCode);
   await page.click("//select[@placeholder='Enter country']");
   await page.selectOption("//select[@placeholder='Enter country']",'India')
   await page.waitForSelector("//button[normalize-space()='Submit']")
@@ -51,4 +63,6 @@ test('Scopex Money End-to-End Test', async ({ page }) => {
 
   //closing browser
   await page.close();
+});
+});
 });
